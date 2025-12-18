b20cbf76dfb0e61e90e0e6e8eac3e49fc116f29e
SlavaVolkov1996git config --global user.name SlavaVolkov1996git config --global user.name SlavaVolkov1996 <slava.volkov.1996@gmail.com>
2025-12-18 16:31:03 +0300

    добавил комментарии ,чтоб на виду были

--- backend/tasks/urls.py
+++ backend/tasks/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import TaskViewSet, CategoryViewSet
 
+# локальные маршруты
 router = DefaultRouter()
 router.register('tasks', TaskViewSet)
 router.register('categories', CategoryViewSet)
--- backend/tasks/views.py
+++ backend/tasks/views.py
@@ -4,25 +4,28 @@ from .models import Task, Category
 from .serializers import TaskSerializer, CategorySerializer
 
 
+# CRUD благодаря ViewSet
 class CategoryViewSet(viewsets.ModelViewSet):
     # объединяем логику для категорий
-    queryset = Category.objects.all()
-    serializer_class = CategorySerializer
+    queryset = Category.objects.all()  # работаем со всеми категориями.
+    serializer_class = CategorySerializer  # используем созданный сериализатор.
 
 
 class TaskViewSet(viewsets.ModelViewSet):
     # объединяем логику для задач
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
-    # фильтрация(немного приятностей)
-    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
-    filterset_fields = ['user_id', 'categories']
-    search_fields = ['title', 'description']
-    ordering_fields = ['created_at', 'due_date']
+    # фильтрация
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter,
+                       filters.OrderingFilter]  # список классов для фильтрации, поиска и сортировки.
+    filterset_fields = ['user_id', 'categories']  # поля фильтрации
+    search_fields = ['title', 'description']  # поля текстового поиска
+    ordering_fields = ['created_at', 'due_date']  # поля сортировки
 
-    def get_queryset(self):
-        queryset = super().get_queryset()
-        user_id = self.request.query_params.get('user_id')
-        if user_id:
-            queryset = queryset.filter(user_id=user_id)
-        return queryset
+    # для аутентификации
+    # def get_queryset(self):
+    ####     return queryset = super().get_queryset()
+    #     user_id = self.request.query_params.get('user_id')
+    #     if user_id:
+    #         queryset = queryset.filter(user_id=user_id)
+    ####     return queryset
